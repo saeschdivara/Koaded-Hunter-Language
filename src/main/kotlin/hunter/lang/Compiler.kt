f77858de8b0b65df308b105e272f874886b1e606
Sascha Häusler <saeschdivara@gmail.com>
2022-01-28 15:56:58 +0100

    Compile submodules

--- src/main/kotlin/hunter/lang/Compiler.kt
+++ src/main/kotlin/hunter/lang/Compiler.kt
@@ -2,13 +2,46 @@ package hunter.lang
 
 import org.kodein.di.DI
 import org.kodein.di.instance
+import java.io.File
 
 class Compiler(di: DI) {
     private val lexer: Lexer by di.instance()
     private val parser: Parser by di.instance()
 
+    private var mainModule: Ast = Ast(emptyList())
+    private var subModules = ArrayList<Ast>()
+
     fun compileModule(path: String) {
 
+        val mainPath = File(path).parent
+        val tokens = lexer.lexFile(path)
+
+        tokens.forEach {
+            println(it)
+        }
+
+        val expressions = parser.parseTokens(tokens)
+        mainModule = Ast(expressions)
+
+        println("")
+        println("Ast:")
+        mainModule.print()
+        expressions.forEach {
+            if (it is ImportExpression) {
+                val submodulePath = resolveSubmodule(it.importPath)
+                val submodule = compileSubModule("$mainPath/$submodulePath")
+                subModules.add(submodule)
+            }
+        }
+    }
+
+    private fun resolveSubmodule(importPath: List<Token>) : String {
+        return importPath.joinToString(separator = "/") {
+            it.lexeme
+        } + ".hunt"
+    }
+
+    private fun compileSubModule(path: String) : Ast {
         val tokens = lexer.lexFile(path)
 
         tokens.forEach {
@@ -17,12 +50,8 @@ class Compiler(di: DI) {
 
         val expressions = parser.parseTokens(tokens)
         val ast = Ast(expressions)
-
-        println("")
-        println("Ast:")
         ast.print()
-//        expressions.forEach {
-//            println(it)
-//        }
+
+        return ast
     }
 }
